34c9706174acc1fd70982493144baeeb63c7b5f3
Davide Forlani <davide.forlani@overweb.it>
2023-05-10 21:09:57 +0200

    Added volume for microphone.

--- src/actions/volume.rs
+++ src/actions/volume.rs
@@ -7,40 +7,24 @@ use crate::{command_lines, Result};
 
 const GET_VOLUME_ARGS: [&str; 2] = ["get-sink-volume", "@DEFAULT_SINK@"];
 const GET_MUTE_ARGS: [&str; 2] = ["get-sink-mute", "@DEFAULT_SINK@"];
-
+const GET_MIC_VOLUME_ARGS: [&str; 2] = ["get-source-volume", "@DEFAULT_SOURCE@"];
+const GET_MIC_MUTE_ARGS: [&str; 2] = ["get-source-mute", "@DEFAULT_SOURCE@"];
 #[derive(Serialize)]
 struct Status {
     volume: usize,
     mute: bool,
+    mic_volume: usize,
+    mic_mute: bool,
 }
 
 impl Status {
     fn new() -> Result<Self> {
-        // Get current volum
-        let cmd = Command::new("pactl").args(GET_VOLUME_ARGS).output()?;
-        let stdout = String::from_utf8(cmd.stdout)?;
-        let regex = regex!("([0-9]+)%");
-        let volumes: Vec<usize> = regex
-            .captures_iter(&stdout)
-            .map(|x| x.get(1))
-            .filter(|x| x.is_some())
-            .map(|x| x.expect("This list should have filtered out None!"))
-            .map(|x| x.as_str().parse())
-            .map(|x| x.expect("The first capture group of the regex is an integer, right?"))
-            .collect();
-        let volume = volumes.iter().sum::<usize>() / volumes.len();
-        // Get current muteness
-        let cmd = Command::new("pactl").args(GET_MUTE_ARGS).output()?;
-        let stdout = String::from_utf8(cmd.stdout)?;
-        let mute = match stdout.trim() {
-            "Mute: no" => false,
-            "Mute: yes" => true,
-            _ => {
-                eprintln!("Getting mute error: found {:?}, expected 'Mute: (yes,no)'", &stdout);
-                false
-            }
-        };
-        Ok(Self { volume, mute })
+        // Get values for speaker
+        let volume = extract_volume_from_pactl(GET_VOLUME_ARGS)?;
+        let mute = extract_mute_from_pactl(GET_MUTE_ARGS)?;
+        let mic_volume = extract_volume_from_pactl(GET_MIC_VOLUME_ARGS)?;
+        let mic_mute = extract_mute_from_pactl(GET_MIC_MUTE_ARGS)?;
+        Ok(Self { volume, mute, mic_volume, mic_mute })
     }
 
     fn print() -> Result<()> {
@@ -49,6 +33,42 @@ impl Status {
         println!("{}", json);
         Ok(())
     }
+}
+
+fn extract_volume_from_pactl(args: [&str; 2]) -> Result<usize> {
+    let cmd = Command::new("pactl").args(args).output()?;
+    let stdout = String::from_utf8(cmd.stdout)?;
+    let regex = regex!("([0-9]+)%");
+    let volumes: Vec<usize> = regex
+        .captures_iter(&stdout)
+        .flat_map(|x| x.get(1))
+        .flat_map(|x| x.as_str().parse().ok())
+        .collect();
+    let volume = match volumes.len() {
+        0 => {
+            eprintln!("Weird, no volume? {}", &stdout);
+            0
+        }
+        l => volumes.iter().sum::<usize>() / l,
+    };
+    Ok(volume)
+}
+
+fn extract_mute_from_pactl(args: [&str; 2]) -> Result<bool> {
+    let cmd = Command::new("pactl").args(args).output()?;
+    let stdout = String::from_utf8(cmd.stdout)?;
+    let mute = match stdout.trim() {
+        "Mute: no" => false,
+        "Mute: yes" => true,
+        _ => {
+            eprintln!(
+                "Getting mute error: found {:?}, expected 'Mute: (yes,no)'",
+                &stdout
+            );
+            false
+        }
+    };
+    Ok(mute)
 }
 
 pub fn watch() -> Result<()> {
